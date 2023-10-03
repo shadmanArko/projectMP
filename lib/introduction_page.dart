3c2690117ef45d2d52e997c5bac1a888e54e10fa
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-10-03 21:50:12 +0600

    Update

--- lib/introduction_page.dart
+++ lib/introduction_page.dart
@@ -12,18 +12,34 @@ class IntroductionPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // Person's name
             Text(
-              'Welcome to the App!',
+              'PROF. DR. PRAN GOPAL DATTA',
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
+            // Person's picture
+            Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                      'assets/person_image.png'), // Replace with your image asset
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Start button
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) => MyHomePage(),
                 ));
               },
-              child: Text('Start'),
+              child: Text('Proceed'),
             ),
           ],
         ),
--- lib/thank_you_page.dart
+++ lib/thank_you_page.dart
@@ -5,24 +5,59 @@ class ThankYouPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Thank You'),
+        title: Text(
+          'Thank You',
+          style: TextStyle(
+            color: Colors.white, // Change the text color to white
+          ),
+        ),
+        backgroundColor: Color.fromRGBO(
+            66, 165, 245, 1), // Change the app bar background color
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Thank you for submitting the form!',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Navigate back to the form page
-              },
-              child: Text('Back to Form'),
-            ),
-          ],
+      body: Container(
+        color: Colors.white, // Change the background color of the body
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.check_circle_outline,
+                color: Color.fromRGBO(
+                    66, 165, 245, 1), // Change the icon color to green
+                size: 80.0, // Increase the icon size
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Thank you for your message!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold, // Apply bold font style
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'We have received your message and will get back to you soon.',
+                textAlign: TextAlign.center, // Center-align the text
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey, // Change the text color to grey
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Navigate back to the form page
+                },
+                child: Text('Back to Form'),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(
+                      66, 165, 245, 1), // Change the button color to teal
+                  onPrimary:
+                      Colors.white, // Change the button text color to white
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
